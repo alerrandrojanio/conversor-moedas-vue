63e166864bb92d78186c9ba620a655f84d59f51f
Alerrandro Janio <62570105+alerrandrojanio@users.noreply.github.com>
2022-08-03 13:42:31 -0300

    update

--- src/components/Conversor.vue
+++ src/components/Conversor.vue
@@ -2,7 +2,7 @@
     <div class="conversor">
         <h2>{{moeda1}} para {{moeda2}}</h2>
         <input type="text" class="form-label" v-model="moeda1_valor" v-bind:placeholder="moeda1">
-        <input type="button" value="Converter" v-on:click="converter">
+        <input type="button" value="Converter" v-on:click="mounted()">
         <h2>{{moeda2_valor}}</h2>
     </div>
 </template>
@@ -17,27 +17,39 @@ export default {
         return{
             moeda1_valor: "",
             moeda2_valor: 0,
-            
+            conversor: []
         };
     },
 
     methods:{
 
+        mounted(){
+            axios.get('https://economia.awesomeapi.com.br/json/last/USD-BRL')
+            .then(response => (this.conversor = response))
+
+            console.log(this.conversor['USDBRL'])
+        },
+
         converter(){
-
             let dePara = this.moeda1 + "-" + this.moeda2
-            let url="https://economia.awesomeapi.com.br/json/last/"+ dePara
-            let conversor = []
+            let url='https://economia.awesomeapi.com.br/json/last/'+ dePara
+            
+            //console.log(url)
 
-            axios.get(url).then((result) =>{
-                conversor = result.data
-            }).catch(function(error) {
-                console.log(error)
-            })
+            axios.all([
+                axios.get(url)
+            ]).then(axios.spread((urlRes)=>{
+                this.conversor = urlRes.data   
+            }))
+            
+            // conversor = axios.get(url)
+            //     .then(axios.spread((urlRes)=>{ 
+            //     conversor = urlRes.data
+            // }))
 
-            console.log(conversor)
-
-            this.moeda2_valor = this.moeda1_valor * parseFloat(this.conversor.high)
+            console.log(this.conversor)
+            //console.log(conversor.high)
+            //this.moeda2_valor = this.moeda1_valor * parseFloat(this.conversor.high)
 
         }
 
